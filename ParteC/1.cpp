0000ae8a9a7a1da5bcb95dd6b6360b0ce68e48ff
Eduardo Almeida <eapsa@ua.pt>
2021-12-26 22:45:57 +0000

    parteC yuv to bgr

--- ParteC/1.cpp
+++ ParteC/1.cpp
@@ -15,29 +15,30 @@ Mat converted_image;
 Mat original_image;
 Mat y,cb,cr,cb2;
 Mat pred_y,pred_cb,pred_cr;
+Mat dec_y,dec_cb,dec_cr;
+Mat desp_y,desp_cb,desp_cr;
+Mat resultYUV;
+Mat result;
 
 void BGRtoYUV420(){
     cvtColor(original_image, converted_image, COLOR_BGR2YUV_I420);
     y = converted_image(Range(0,converted_image.rows*2/3), Range(0,converted_image.cols));
     cb = converted_image(Range(converted_image.rows*2/3,converted_image.rows*2/3+((converted_image.rows-converted_image.rows*2/3)/2)),
-        Range(converted_image.cols/2,converted_image.cols));
-    cb2 = converted_image(Range(converted_image.rows*2/3,converted_image.rows*2/3+((converted_image.rows-converted_image.rows*2/3)/2)),
-        Range(0,converted_image.cols/2));
+        Range(0,converted_image.cols));
     cr = converted_image(Range(converted_image.rows*2/3+((converted_image.rows-converted_image.rows*2/3)/2),converted_image.rows), 
-        Range(converted_image.cols/2,converted_image.cols));
-    imshow("y",y);
-    imshow("cb",cb);
-    imshow("cr",cr);
-    imshow("cb2",cb2);
-    for(int x=0; x<cb.rows; x++){
-        for(int y=0; y<cb.cols; y++){
-            int x1 = cb2.at<int>(x,y);
-            int x2 = cb.at<int>(x,y);
-            //cout << x1 << " X " << x2  << '\n';
-        }
-    }
+        Range(0,converted_image.cols));
+    // imshow("y",y);
+    // imshow("cb",cb);
+    // imshow("cr",cr);
  }
 
+
+ void YUV420toBGR(){
+     desp_y.copyTo(resultYUV(Range(0, desp_y.rows),Range(0,desp_y.cols)));
+     desp_cb.copyTo(resultYUV(Range(desp_y.rows, (desp_y.rows+desp_cb.rows)),Range(0,desp_cb.cols)));
+     desp_cr.copyTo(resultYUV(Range((desp_y.rows+desp_cb.rows), resultYUV.rows),Range(0,desp_cr.cols)));
+     cvtColor(resultYUV, result, COLOR_YUV2BGR_I420);
+}
 
 void predictorJPEGls(Mat img, Mat &predictor){
     int a,b,c;
@@ -46,25 +47,52 @@ void predictorJPEGls(Mat img, Mat &predictor){
             a = 0;
             b = 0;
             c = 0;
-            if(x!=0 && y==0) b = img.at<uchar>(x-1,y);
-            else if(x==0 && y!=0) a = img.at<uchar>(x,y-1);
+            if(x!=0 && y==0) b = (int)img.at<char>(x-1,y);
+            else if(x==0 && y!=0) a = (int)img.at<char>(x,y-1);
             else if(x!=0 && y!=0){
-                a = img.at<uchar>(x,y-1);
-                b = img.at<uchar>(x-1,y);
-                c = img.at<uchar>(x-1,y-1);
+                a = (int)img.at<char>(x,y-1);
+                b = (int)img.at<char>(x-1,y);
+                c = (int)img.at<char>(x-1,y-1);
             }
             int prev = 0;
             if(c>=max(a,b)) prev = min(a,b);
             else if(c<=min(a,b)) prev = max(a,b);
             else prev = a+b-c;
 
-            int pixel = img.at<uchar>(x,y);
-            predictor.at<uchar>(x,y) = pixel - prev;
+            int pixel = img.at<char>(x,y);
+            predictor.at<char>(x,y) = pixel - prev;
             
             //cout << "pixel " << pixel << " X " << prev << " - " << pixel - prev <<'\n';
         }
     }
-    //cout << (int)predictor.at<uchar>(0,0) << " - " << (int)img.at<uchar>(0,0) <<'\n';
+    //cout << (int)predictor.at<char>(0,0) << " - " << (int)img.at<char>(0,0) <<'\n';
+}
+
+void despredictor(Mat img, Mat &despredictor){
+    int a,b,c;
+    for(int x=0; x<img.rows; x++){
+        for(int y=0; y<img.cols; y++){
+            a = 0;
+            b = 0;
+            c = 0;
+            if(x!=0 && y==0) b = (int)despredictor.at<char>(x-1,y);
+            else if(x==0 && y!=0) a = (int)despredictor.at<char>(x,y-1);
+            else if(x!=0 && y!=0){
+                a = (int)despredictor.at<char>(x,y-1);
+                b = (int)despredictor.at<char>(x-1,y);
+                c = (int)despredictor.at<char>(x-1,y-1);
+            }
+            int prev = 0;
+            if(c>=max(a,b)) prev = min(a,b);
+            else if(c<=min(a,b)) prev = max(a,b);
+            else prev = a+b-c;
+
+            despredictor.at<char>(x,y) = (int)img.at<char>(x,y) + prev;
+            //cout << (int)despredictor.at<char>(x,y) << " " << (int)converted_image.at<char>(x,y) << '\n';
+            //cout << a << ' ' << b << ' ' << c << ' ' << (int)img.at<char>(x,y) << ' ' << prev <<'\n';
+            //cout << y << " next " << '\n';
+        }
+    }
 }
 
 void encode(int m, char* file){
@@ -74,46 +102,49 @@ void encode(int m, char* file){
     g.openBsw();
     for(int x=0; x<pred_y.rows; x++){
         for(int y=0; y<pred_y.cols; y++){
-            cout << (int)pred_y.at<char>(x,y) << '\n';
+            //cout << (int)pred_y.at<char>(x,y) << '\n';
             g.encode(pred_y.at<char>(x,y));
         }
     }
-    // for(int x=0; x<pred_cb.rows; x++){
-    //     for(int y=0; y<pred_cb.cols; y++){
-    //         //cout << (int)pred_cb.at<char>(x,y) << '\n';
-    //         g.encode(pred_cb.at<char>(x,y));
-    //     }
-    // }
-    // for(int x=0; x<pred_cr.rows; x++){
-    //     for(int y=0; y<pred_cr.cols; y++){
-    //         //cout << (int)pred_cr.at<char>(x,y) << '\n';
-    //         g.encode(pred_cr.at<char>(x,y));
-    //     }
-    // }
+    for(int x=0; x<pred_cb.rows; x++){
+        for(int y=0; y<pred_cb.cols; y++){
+            //cout << (int)pred_cb.at<char>(x,y) << '\n';
+            g.encode(pred_cb.at<char>(x,y));
+        }
+    }
+    for(int x=0; x<pred_cr.rows; x++){
+        for(int y=0; y<pred_cr.cols; y++){
+            //cout << (int)pred_cr.at<char>(x,y) << '\n';
+            g.encode(pred_cr.at<char>(x,y));
+        }
+    }
     g.closeBsw();
 }
 
-void decode(int m, char* file){
+void decode(int m, char* file, Mat &dy, Mat &dcb, Mat &dcr){
     Golomb g(m,file);
     g.openBsr();
     for(int x=0; x<pred_y.rows; x++){
         for(int y=0; y<pred_y.cols; y++){
             char a = g.decode();
-            cout << (int)a << " x " <<(int)pred_y.at<char>(x,y)<< '\n';
+            dy.at<char>(x,y) = a;
+            //cout << (int)dec_y.at<char>(x,y) << " x " <<(int)pred_y.at<char>(x,y)<< '\n';
         }
     }
-    // for(int x=0; x<pred_cb.rows; x++){
-    //     for(int y=0; y<pred_cb.cols; y++){
-    //         //cout << (int)pred_cb.at<char>(x,y) << '\n';
-    //         g.encode(pred_cb.at<char>(x,y));
-    //     }
-    // }
-    // for(int x=0; x<pred_cr.rows; x++){
-    //     for(int y=0; y<pred_cr.cols; y++){
-    //         //cout << (int)pred_cr.at<char>(x,y) << '\n';
-    //         g.encode(pred_cr.at<char>(x,y));
-    //     }
-    // }
+    for(int x=0; x<pred_cb.rows; x++){
+        for(int y=0; y<pred_cb.cols; y++){
+            char a = g.decode();
+            dcb.at<char>(x,y) = a;
+            //cout << (int)dec_cb.at<char>(x,y) << " x " <<(int)pred_cb.at<char>(x,y)<< '\n';
+        }
+    }
+    for(int x=0; x<pred_cr.rows; x++){
+        for(int y=0; y<pred_cr.cols; y++){
+            char a = g.decode();
+            dcr.at<char>(x,y) = a;
+            //cout << (int)dec_cr.at<char>(x,y) << " x " <<(int)pred_cr.at<char>(x,y)<< '\n';
+        }
+    }
     g.closeBsr();
 }
 
@@ -125,13 +156,31 @@ int main(int argc, char** argv){
     cb.copyTo(pred_cb);
     predictorJPEGls(cb,pred_cb);
     cr.copyTo(pred_cr);
-    predictorJPEGls(cr,pred_cb);
+    predictorJPEGls(cr,pred_cr);
 
     encode(6,"teste.txt");
-    decode(6,"teste.txt");
+    y.copyTo(dec_y);
+    cb.copyTo(dec_cb);
+    cr.copyTo(dec_cr);
+    decode(6,"teste.txt",dec_y,dec_cb,dec_cr);
 
-    imshow("Restored", converted_image);
-    imshow("Restd", original_image);
+    dec_y.copyTo(desp_y);
+    despredictor(dec_y,desp_y);
+    dec_cb.copyTo(desp_cb);
+    despredictor(dec_cb,desp_cb);
+    dec_cr.copyTo(desp_cr);
+    despredictor(dec_cr,desp_cr);
+    
+    resultYUV = Mat::zeros(converted_image.rows, converted_image.cols,CV_8U);
+    YUV420toBGR();
+
+    // imshow("r_y",desp_y);
+    // imshow("r_cb",desp_cb);
+    // imshow("r_cr",desp_cr);
+    imshow("original2YUV", converted_image);
+    imshow("Original", original_image);
+    imshow("resultYUV",resultYUV);
+    imshow("result",result);
     waitKey();
     return EXIT_SUCCESS;
 }
